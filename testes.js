df7177cc49291037490d52af31eab16b697d771a
DouglasMuzzi <41204579+DouglasMuzzi@users.noreply.github.com>
2018-07-16 15:56:19 -0300

    Add files via upload

--- testes.js
+++ testes.js
@@ -1,3 +1,11 @@
 function momgay(){
 	window.alert('ur mom gay lol')
 }
+function clonk(){
+	var x = document.getElementById("divman");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
