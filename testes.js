07fab3d97b597d9311d51b239c1f72fa4c7659e8
DouglasMuzzi <41204579+DouglasMuzzi@users.noreply.github.com>
2018-07-17 17:41:12 -0300

    Add files via upload

--- testes.js
+++ testes.js
@@ -5,7 +5,25 @@ function clonk(){
 	var x = document.getElementById("divman");
     if (x.style.display === "block") {
         x.style.display = "none";
-    } else {
+    }
+	else {
         x.style.display = "block";
     }
+}
+function registro(){
+	var x = document.getElementById("erro");
+	var y = document.getElementById("btnregistro");
+	var z =document.getElementById("bom");
+	var nome=document.getElementById("nome");
+	var sobrenome=document.getElementById("sobrenome");
+	var tel=document.getElementById("tel");
+	var voide=document.getElementById("voide");
+	if(!nome||!sobrenome||!tel||!voide){
+		x.style.display = "block";
+		y.style.display = "none";
+	}
+	else{
+		z.style.display = "block";
+
+	}
 }
